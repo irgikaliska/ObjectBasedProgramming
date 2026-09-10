86bcb8b498521b2aa1e2eee538e9b6901ed65339
Irgi Kaliska <irgikaliska24@gmail.com>
2026-09-10 15:23:55 +0700

    Jobsheet 3

---
+++ src/Week03/GetterSetterCooperative/KoperasiDemo.java
@@ -0,0 +1,15 @@
+package Week03.GetterSetterCooperative;
+
+public class KoperasiDemo {
+    public static void main(String[] args) {
+        Anggota anggota1 = new Anggota();
+        anggota1.setNama("Iwan Setiawan");
+        anggota1.setAlamat("Jl. Soekarno Hatta no 10");
+        anggota1.setor(10000);
+        System.out.println("Simpanan " + anggota1.getNama() + " : Rp" + anggota1.getSimpanan());
+
+        anggota1.pinjam(5000);
+        System.out.println("Simpanan " + anggota1.getNama() + " : Rp" + anggota1.getSimpanan());
+
+    }
+}
